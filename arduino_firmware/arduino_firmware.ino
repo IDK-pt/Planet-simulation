dab3640c7f0b72c53d4adac59d970c7ce1468d61
LittleNyanCat <littlenyancat204@gmail.com>
2022-03-14 08:46:19 +0000

    serial commands

--- arduino_firmware/arduino_firmware.ino
+++ arduino_firmware/arduino_firmware.ino
@@ -1,39 +1,90 @@
-// defines pins numbers
-const int stepPin = 11; 
-const int dirPin = 12; 
- 
+#define STEPPING 2
+#define STEP_TIME_MULT 1
+#define STEP_TIME_MICROSECONDS_LOWER 450
+#define STEP_TIME_MICROSECONDS_WAIT 450
+
+
+
+#define STEP_PIN 11
+#define DIR_PIN 12
+#define ENDSTP_PIN 10
+
+bool endstopState = false;
+float currentAngle = 0.0f;
+float targetAngle = 0.0f;
+
 void setup() {
   // Sets the two pins as Outputs
-  pinMode(stepPin,OUTPUT); 
-  pinMode(dirPin,OUTPUT);
+  pinMode(STEP_PIN,OUTPUT); 
+  pinMode(DIR_PIN,OUTPUT);
+  pinMode(ENDSTP_PIN, INPUT);
+
+  Serial.begin(115200);
+
+  doHome();
 }
 void loop() {
-  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
-  // Makes 200 pulses for making one full cycle rotation
-  for(int x = 0; x < 200; x++) {
-    digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(500); 
-    digitalWrite(stepPin,LOW); 
-    delayMicroseconds(500); 
-    led();
+  if (Serial.available()){
+    String recieveString = Serial.readStringUntil('\n');
+    if (recieveString.startsWith("A")) {
+      
+      setAngle(recieveString.substring(1).toFloat());
+    }
+    if (recieveString.startsWith("B")) {
+      
+      int tgtAngle = recieveString.substring(1).toInt();
+      if (tgtAngle == 360){
+        setAngle(360.0f);
+        currentAngle = 0.0f;
+        targetAngle = 0.0f;
+      }
+      else{
+        setAngle(recieveString.substring(1).toFloat());
+      }
+      
+    }
+
+   
   }
-  led();
-  delay(1000); // One second delay
-  led();
-  digitalWrite(dirPin,LOW); //Changes the rotations direction
-  // Makes 400 pulses for making two full cycle rotation
-  for(int x = 0; x < 400; x++) {
-    digitalWrite(stepPin,HIGH);
-    delayMicroseconds(500);
-    digitalWrite(stepPin,LOW);
-    delayMicroseconds(500);
-    led();
-  }
-  led();
-  delay(1000);
-  led();
 }
 
-void led(){
-  digitalWrite(LED_BUILTIN,digitalRead(10));
+void setAngle(float angle){
+  targetAngle = angle;
+
+  Serial.print("current:");
+    Serial.println(currentAngle);
+    Serial.print("target:");
+    Serial.println(targetAngle);
+  Serial.print("delta:");
+    Serial.println(targetAngle - currentAngle);
+     Serial.println();
+  rotateAngle(targetAngle - currentAngle);
+}
+
+void rotateAngle(float angle){
+  bool dir = angle > 0;
+  for(int i = 0; i < abs(angle)/(1.8/STEPPING); i++){
+    doStep(dir);
+  }
+  if(targetAngle != currentAngle){
+    currentAngle += angle;
+  }
+}
+
+void doStep(bool dir){
+digitalWrite(DIR_PIN, dir);
+digitalWrite(STEP_PIN,HIGH);
+delayMicroseconds(STEP_TIME_MICROSECONDS_LOWER * STEP_TIME_MULT);
+digitalWrite(STEP_PIN,LOW);
+delayMicroseconds(STEP_TIME_MICROSECONDS_WAIT * STEP_TIME_MULT);
+}
+
+void doHome(){
+  for(int i = 0; i < 200*STEPPING; i++){
+    doStep(true);
+    if(digitalRead(ENDSTP_PIN)){
+      break;
+    }
+  }
+  currentAngle = 0.0f;
 }
